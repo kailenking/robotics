8814ac8718f54078dbbdc6efdee801d93a9a0457
khking <khking@email.sc.edu>
2021-10-27 20:45:05 -0400

    homework 4

---
+++ ros_starters_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /code/catkin_ws/src/robotics/ros_starters_ws/src
+# Build directory: /code/catkin_ws/src/robotics/ros_starters_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("luqman_package")
---
+++ ros_starters_ws/build/luqman_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /code/catkin_ws/src/robotics/ros_starters_ws/src/luqman_package
+# Build directory: /code/catkin_ws/src/robotics/ros_starters_ws/build/luqman_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
