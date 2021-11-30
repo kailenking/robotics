bd85fe575cb5d0292fa456f70adb4e17a57a4a2e
kailenking <kailenking75@gmail.com>
2021-11-30 00:39:24 +0000

    square

---
+++ packages/project3/src/square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+#
+import rospy
+from duckietown_msgs.msg import Twist2DStamped
+from time import sleep
+
+def projectsquare():
+    pub = rospy.Publisher('car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
+    rospy.init_node('project3square')
+    msg = Twist2DStamped(header=None, v=1.0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=6)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=1.0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=6)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=1.0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=6)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=1.0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=6)
+    pub.publish(msg)
+    sleep(1)
+    msg = Twist2DStamped(header=None, v=0, omega=0)
+    pub.publish(msg)
+
+
+if __name__ == '__main__':
+    try:
+     projectsquare()
+    except rospy.ROSInterruptException:
+       pass
