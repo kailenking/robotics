dce476698337eaa32aecc4beea0b19138b1259c2
kailenking <kailenking75@gmail.com>
2021-11-29 19:50:31 -0500

    duplicated directory by accident

--- robotics/ros_starters_ws/build/luqman_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /code/catkin_ws/src/robotics/ros_starters_ws/src/luqman_package
-# Build directory: /code/catkin_ws/src/robotics/ros_starters_ws/build/luqman_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- robotics/ros_starters_ws/build/luqman_package/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "luqman_package"
-PROJECT_SPACE_DIR = "/code/catkin_ws/src/robotics/ros_starters_ws/devel"
-PROJECT_VERSION = "0.0.0"
